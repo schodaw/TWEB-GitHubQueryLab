c0d887aa4f572a0119b9c53706b335722e9d4e2b
reds <dominique.jollien@heig-vd.ch>
2014-10-06 16:54:24 +0200

    fix multiple request issue

--- public/javascripts/labo3.js
+++ public/javascripts/labo3.js
@@ -9,23 +9,6 @@
  -------------------------------------------------------------------------------
  */
 $(function(){
-    
-    var context = { 
-        tableHeaderId: "id",
-        tableHeaderName: "nom",
-        tableHeaderDescription: "description",
-        tableHeaderOwner: "propriétaire",
-        tableHeaderStars: "étoiles"
-    };
-
-    //getting the handlebar templates for github research queries
-    var resultHeaderSource = $("#result-header-template").html()
-    var resultHeaderTemplate = Handlebars.compile(resultHeaderSource); 
-    $('tr#result-header').html(resultHeaderTemplate(context));
-    
-    var resultSource = $("#result-template").html();
-    var resultTemplate = Handlebars.compile(resultSource);
-    
     $("#searchButton").click(function(){
         //prevent default handling of the event
         //we do that so that the page is not refreshed which would erase the content of the result <p> we just put with AJAX
@@ -35,11 +18,14 @@ $(function(){
         //this method doesn't work on cross-domain url
         $.getJSON("/search", "query=" + $("#searchQuery").val(), function( data ) {
             
-            //use handlebars to put the query result in the UI
-            //$('table#result').html("");
+            //getting the handlebar templates for github research queries    
+            var resultSource = "<tr><td>{{id}}</td><td>{{full_name}}</td><td>{{description}}</td><td>{{owner.login}}</td><td>{{stars}}</td></tr>";
+            var resultTemplate = Handlebars.compile(resultSource);
             
+            //use handlebars to put the query result in the UI
+            $('table#result tbody').empty();
             data.repositories.forEach(function(repo) {
-                $('table#result').html($('table#result').html() + resultTemplate(repo));
+                $('table#result tbody').html($('table#result tbody').html() + resultTemplate(repo));
             });
         });
     });
